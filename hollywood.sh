ffd710fbb5cc6d01e0c636cc5439eff9dc9f37fb
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-05-10 10:00:16 +0800

    Update hollywood.sh

--- hollywood.sh
+++ hollywood.sh
@@ -5,12 +5,16 @@ OS="$(awk -F= '/^NAME/{print $2}' /etc/os-release)"
 
 # CentOS 系统
 if [[ $OS == *"CentOS"* ]]; then
+sudo yum clean all
+sudo yum makecache fast
     # 安装 EPEL 仓库
     sudo yum install -y epel-release
     # 安装 hollywood
     sudo yum install -y hollywood
 # Debian 或 Ubuntu 系统
 elif [[ $OS == *"Debian"* ]] || [[ $OS == *"Ubuntu"* ]]; then
+sudo apt clean all
+sudo apt makecache fast
     # 更新软件包列表
     sudo apt-get update
     # 安装 hollywood
