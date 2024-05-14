d3a8a70f94473bd5911307fccf59056c42abcb7b
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-05-14 13:23:12 +0800

    Update someinteresting.sh

--- someinteresting.sh
+++ someinteresting.sh
@@ -22,19 +22,53 @@ install_package() {
 
 case $choice in
     1)
-        perl -MCPAN -e 'install Term::Animation'
-        sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install libncurses5-dev
-cpan Curses
-wget http://www.robobunny.com/projects/asciiquarium/asciiquarium.tar.gz
-tar -zxvf asciiquarium.tar.gz
-cd asciiquarium_*/
-sudo cp asciiquarium /usr/local/bin
-sudo chmod 0755 /usr/local/bin/asciiquarium
 
+# 定义安装 Perl Curses 模块的函数
+install_perl_curses_module() {
+    cpan App::cpanminus
+    cpanm Curses
+}
+
+# 定义安装 asciiquarium 的函数
+install_asciiquarium() {
+    wget http://www.robobunny.com/projects/asciiquarium/asciiquarium.tar.gz
+    tar zxvf asciiquarium.tar.gz
+    cd asciiquarium_*/
+    chmod +x asciiquarium
+    sudo cp asciiquarium /usr/local/bin
+}
+
+# 判断操作系统并安装依赖
+if [ -f /etc/os-release ]; then
+    # 加载操作系统信息
+    . /etc/os-release
+    
+    if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
+        # Debian 或 Ubuntu
+        sudo apt-get update
+        sudo apt-get install -y cpanminus wget libncurses5-dev make gcc
+        install_perl_curses_module
+        install_asciiquarium
+
+    elif [ "$ID" = "centos" ]; then
+        # CentOS
+        sudo yum check-update
+        sudo yum install -y perl-CPAN wget ncurses-devel gcc make
+        install_perl_curses_module
+        install_asciiquarium
+    else
+        echo "此脚本不支持的Linux发行版：$ID"
+        exit 1
+    fi
+else
+    echo "无法确定操作系统。"
+    exit 1
+fi
+
+echo "asciiquarium 安装完成，可以通过运行 'asciiquarium' 来启动。"
         asciiquarium
         ;;
+        
     2)
         install_package "cmatrix"
         cmatrix
